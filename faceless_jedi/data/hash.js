e76c8246587d354a8ed39f59b4e8e2bd76ed6e43
Sonya Pinero <sonyapinero@gmail.com>
2015-05-24 19:15:24 -0400

    reddit parse by jpg

--- faceless_jedi/data/hash.js
+++ faceless_jedi/data/hash.js
@@ -1,13 +1,30 @@
+// var https = require('https');
+
 var json = require('./hash.json');
 
-var url = json.data.children[0].data.url
+// var url = json.data.children[0].data.url
+
+// for (var key in json) {
+//   if (json.hasOwnProperty(key)) {
+//     for (var i = 0; i < json.data.children.length; i++){
+//     	console.log(json.data.children[i].data.url);
+//     }
+  
+//   }
+// }
+
+
 
 for (var key in json) {
   if (json.hasOwnProperty(key)) {
     for (var i = 0; i < json.data.children.length; i++){
-    	console.log(json.data.children[i].data.url);
+    	if (json.data.children[i].data.url.indexOf("jpg") > -1){
+    		console.log(json.data.children[i].data.url);
+    	}    	
     }
-  
   }
 }
+
+// var site = 'http://www.reddit.com/r/ImaginaryWesteros/.json'
+
 
